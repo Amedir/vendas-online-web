df016eb088b6d1f6d6d525bf069c511053201fcb
AdemirMont <ademir.monteiro@elogroup.com.br>
2026-05-27 16:43:49 -0300

    feat: create module login

---
+++ src/modules/login/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import { BackgroudImage } from '../styles/loginScreen.styles';
+
+const LoginScreen = () => {
+  return (
+    <div>
+      <BackgroudImage src="./background.jpg" alt="Hero Image" />
+    </div>
+  );
+};
+
+export default LoginScreen;
