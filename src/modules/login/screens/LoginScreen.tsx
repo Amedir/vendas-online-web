19d3bcdce6a614922ca722d50bb0a6497fdf1b8e
AdemirMont <ademir.monteiro@elogroup.com.br>
2026-05-27 22:48:34 -0300

    feat: create login component

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,20 @@
-import { BackgroudImage } from '../styles/loginScreen.styles';
+import {
+  BackgroudImage,
+  Container,
+  LimitedContainer,
+  LogoImage,
+} from '../styles/loginScreen.styles';
 
 const LoginScreen = () => {
   return (
     <div>
       <BackgroudImage src="./background.jpg" alt="Hero Image" />
+
+      <Container>
+        <LimitedContainer>
+          <LogoImage src="./logo.png" alt="Logo" />
+        </LimitedContainer>
+      </Container>
     </div>
   );
 };
